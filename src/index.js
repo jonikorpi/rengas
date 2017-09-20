5e678152210cf61a8d276262e3384ae23ed24fad
Joni Korpi <me@jonikorpi.com>
2017-09-20 15:29:26 +0300

    More initial setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import App from "./components/App";
+// import registerServiceWorker from "./registerServiceWorker";
 
 import "./reset.css";
 import "./globals.css";
+import "./components.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
