aeb210ff0386e836c1adf2f48d339d9f92ebe775
Joni Korpi <me@jonikorpi.com>
2017-09-27 15:34:30 +0300

    Avoids using transforms on tiles

--- src/components/World.js
+++ src/components/World.js
@@ -1,23 +1,23 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import { rules } from "../shared/helpers.js";
 
-export default class World extends PureComponent {
-  render() {
-    return (
-      <div
-        className="world"
-        style={{
-          "--tileSize": 100 / rules.worldWidth,
-          "--worldLength": this.props.worldLength,
-          "--worldWidth": rules.worldWidth,
-          "--plateLength": rules.plateLength,
-          "--minimumPlateLength": rules.minimumPlateLength,
-        }}
-      >
-        {this.props.children}
+const World = ({ children, worldLength }) => {
+  return (
+    <div
+      className="world"
+      style={{
+        "--tileSize": 100 / rules.worldWidth,
+        "--worldLength": worldLength,
+        "--worldWidth": rules.worldWidth,
+        "--plateLength": rules.plateLength,
+        "--minimumPlateLength": rules.minimumPlateLength,
+      }}
+    >
+      <div className="worldSizer">{children}</div>
 
-        <div className="worldShadow" />
-      </div>
-    );
-  }
-}
+      <div className="worldShadow" />
+    </div>
+  );
+};
+
+export default World;
