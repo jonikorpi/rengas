d88d6c9742f0484240fca850e6e4d6a614a612f6
Joni Korpi <me@jonikorpi.com>
2017-09-27 19:18:29 +0300

    Makes SVG accept style and className

--- src/components/SVG.js
+++ src/components/SVG.js
@@ -5,15 +5,17 @@ import { config } from "../graphics.js";
 const tileWidth = config.tileSize * config.tileCanvasMultiplier;
 const viewBox = `-${tileWidth / 2} -${tileWidth / 2} ${tileWidth} ${tileWidth}`;
 
-const SVG = ({ children, z }) => {
+const SVG = ({ children, z, zIndex, style, className }) => {
   return (
     <svg
-      className="svg"
+      className={"svg " + className}
       shapeRendering="optimizeSpeed"
       preserveAspectRatio="none"
       viewBox={viewBox}
       style={{
         "--z": z || 0,
+        zIndex: zIndex || z || 0,
+        ...style,
       }}
     >
       {children}
