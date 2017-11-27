52023a92ddf2c1d96e742c72d4ad3bb6d90f31c9
Joni Korpi <me@jonikorpi.com>
2017-11-27 20:44:27 +0200

    Adds error message to Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Loader = ({ error, pastDelay, timedOut }) => "Downloading components…";
+const Loader = ({ error, pastDelay, timedOut }) => {
+  const message =
+    error || timedOut
+      ? "Component download failed. Please refresh!"
+      : "Downloading components…";
+
+  return pastDelay ? message : null;
+};
 
 export default Loader;
