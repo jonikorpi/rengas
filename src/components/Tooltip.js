7aa5756bb6bbcf3f65ff10b918705861368f439a
Joni Korpi <me@jonikorpi.com>
2017-11-29 18:08:16 +0200

    Matches tooltip text-alignment to tooltip alignment

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,11 +3,16 @@ import React from "react";
 import { rules } from "../shared/helpers.js";
 
 const Tooltip = ({ x, trueSight, unitExists, unitID, unit, tile }) => {
+  const leftAligned = x < rules.worldWidth / 2;
+
   return (
     <div
       className="tooltip"
       role="tooltip"
-      style={{ [x < rules.worldWidth / 2 ? "left" : "right"]: "100%" }}
+      style={{
+        [leftAligned ? "left" : "right"]: "100%",
+        textAlign: !leftAligned && "right",
+      }}
     >
       <h1>{tile.type}</h1>
       {unit && <h1>{unit.type}</h1>}
