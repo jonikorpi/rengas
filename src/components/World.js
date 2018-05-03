2f249d90701dc5706fbed9b98f5447ce9a51a175
Joni Korpi <me@jonikorpi.com>
2018-05-03 18:36:06 +0300

    Adds crappy player movement

--- src/components/World.js
+++ src/components/World.js
@@ -9,32 +9,66 @@ const width = 8;
 const height = 55;
 const grid = createGrid(width, height);
 
-const World = () => {
-  return (
-    <div className="backdrop">
-      <div
-        className="grid"
-        style={{ "--gridWidth": width, "--gridHeight": height }}
-      >
-        {grid.map((row, y) =>
-          row.map((seed, x) => (
-            <div
-              className="static"
-              style={{ "--x": x, "--y": y }}
-              key={`${x},${y}`}
-              title={`${x},${y}`}
-            />
-          ))
-        )}
+class World extends React.Component {
+  state = { x: 2, y: 5 };
 
-        <div className="dynamic" style={{ "--x": 2, "--y": 5, "--z": 1 }}>
-          P
-        </div>
-        <div className="dynamic" style={{ "--x": 2, "--y": 5, color: "red" }}>
-          P
+  render() {
+    const { x, y } = this.state;
+    return (
+      <div className="backdrop">
+        <div
+          className="grid"
+          style={{ "--gridWidth": width, "--gridHeight": height }}
+          onClick={this.moveTo}
+        >
+          {grid.map((row, y) =>
+            row.map((seed, x) => (
+              <div
+                className="static"
+                style={{ "--x": x, "--y": y }}
+                key={`${x},${y}`}
+                title={`${x},${y}`}
+              >
+                <button
+                  type="button"
+                  className="tileButton"
+                  onClick={() =>
+                    this.setState({
+                      x: x,
+                      y: y,
+                    })
+                  }
+                />
+              </div>
+            ))
+          )}
+
+          <Player x={x} y={y} />
         </div>
       </div>
-    </div>
+    );
+  }
+}
+
+const Player = ({ x, y }) => {
+  return (
+    <React.Fragment>
+      <div
+        className="dynamic"
+        style={{ "--x": x, "--y": y, "--z": 2, color: "yellow" }}
+      >
+        P
+      </div>
+      <div
+        className="dynamic"
+        style={{ "--x": x, "--y": y, "--z": 1, color: "orange" }}
+      >
+        P
+      </div>
+      <div className="dynamic" style={{ "--x": x, "--y": y, color: "red" }}>
+        P
+      </div>
+    </React.Fragment>
   );
 };
 
